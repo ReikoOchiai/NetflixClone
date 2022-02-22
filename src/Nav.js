19d06a382f53bb8f2e103e0de4617bf8c8f92bef
ReikoOchiai <lissage.23@gmail.com>
2022-02-22 18:48:59 +1100

    Fixed logo and avatar img

--- src/Nav.js
+++ src/Nav.js
@@ -17,14 +17,10 @@ function Nav() {
 
   return (
     <div className={`nav ${show && "nav_black"}`}>
+      <img src="img/netflix.png" className="nav_logo" alt="Netflix Logo" />
       <img
-        className="nav_logo"
-        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-        alt="Netflix Logo"
-      />
-      <img
+        src="img/netflixMark.png"
         className="nav_avatar"
-        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
         alt="Account Logo"
       />
     </div>
